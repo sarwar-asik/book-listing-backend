11075f4d038a9ecdc88813f0691db4b41605a9a5
sarwar-asik <sarwarasik@gmail.com>
2023-09-01 18:22:08 +0600

    auth added

--- src/app/modules/Users/Users.route.ts
+++ src/app/modules/Users/Users.route.ts
@@ -4,15 +4,18 @@ import { Router } from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { UsersController } from './Users.controller';
 import { UsersValidation } from './Users.validation';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = Router();
-router.get('/', UsersController.getAllDB);
-router.get('/:id', UsersController.getSingleDataById);
+router.get('/',auth(ENUM_USER_ROLE.ADMIN), UsersController.getAllDB);
+router.get('/:id',auth(ENUM_USER_ROLE.ADMIN), UsersController.getSingleDataById);
 router.patch(
-  '/:id',
+  '/:id'
+  ,auth(ENUM_USER_ROLE.ADMIN),
   validateRequest(UsersValidation.updateUser),
   UsersController.updateIntoDb
 );
-router.delete('/:id', UsersController.deleteFromDb);
+router.delete('/:id',auth(ENUM_USER_ROLE.ADMIN), UsersController.deleteFromDb);
 
 export const usersRoutes = router;
