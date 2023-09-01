2b7e015b047449de7a5aab82703403670afd44cf
sarwar-asik <sarwarasik@gmail.com>
2023-09-01 17:32:40 +0600

    signp done

--- src/app/modules/Auth/Auth.services.ts
+++ src/app/modules/Auth/Auth.services.ts
@@ -1,10 +1,14 @@
-import { User } from "@prisma/client"
+import { User } from '@prisma/client';
+import prisma from '../../../shared/prisma';
 
+const insertDb = async (data: User): Promise<User> => {
+  console.log(data);
 
-const insertDb = async (data:User):Promise<User>=>{
-console.log(data);
+  const signUp = await prisma.user.create({
+    data,
+  });
 
-}
+  return signUp;
+};
 
-
-export const AUthService = {insertDb}
+export const AUthService = { insertDb };
